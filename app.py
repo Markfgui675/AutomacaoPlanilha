b4a486bf34d3ffc94bc4370b02ba8ae9c88100ec
Marcos Fellipe Guimarães <85682890+Markfgui675@users.noreply.github.com>
2023-01-13 11:35:53 -0300

    Buscando o arquivo

--- app.py
+++ app.py
@@ -6,3 +6,10 @@ from selenium.webdriver.edge.service import Service
 servico = Service(EdgeChromiumDriverManager().install())
 
 navegador = webdriver.Edge(service=servico)
+
+navegador.get("https://docs.google.com/spreadsheets/d/16SIrhXr4mYiamfQrox9ScSrPhz3LI7RN/edit#gid=1018639743")
+v = navegador.current_url
+if "accounts.google" in v:
+    navegador.find_element('xpath','//*[@id="identifierId"]').send_keys('marcosfgui2006@gmail.com')
+    navegador.find_element('xpath','//*[@id="identifierNext"]/div/button/span').click()
+    sleep(10)
